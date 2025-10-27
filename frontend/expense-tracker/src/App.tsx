f0346ba7ce3326e1f4108f3b40eac7c3fd837328
vishal-git-18 <vishalravishankar18@gmail.com>
2025-10-27 21:46:23 +0530

    Tailwind and React - TS Setup

---
+++ frontend/expense-tracker/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white text-4xl font-bold">
+        Tailwind is working 🎉
+      </div>
+    </>
+  )
+}
+
+export default App
